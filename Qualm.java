ca858a1368b91f7f2b846d96b5023ac5d91ba794
Stephen Peters <portnoy@portnoy.org>
2004-01-12 22:35:16 +0000

    Changed to deal only with ALSA ports

--- Qualm.java
+++ Qualm.java
@@ -1,24 +1,34 @@
 package qualm;
 
 import javax.sound.midi.*;
+import java.util.*;
 
 public class Qualm {
   
   public static void main(String[] args) {
     MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
     if (infos.length == 0) {
-      System.out.println( "No Midi devices found???" );
+      System.out.println( "No MIDI devices found.  Exiting." );
       System.exit(1);
     }
-    
+
+    /* Find ALSA MIDI ports */
+    List midiports = new ArrayList();
     for(int i = 0; i<infos.length; i++) {
-      try { 
-	MidiDevice device = MidiSystem.getMidiDevice(infos[i]);
-	System.out.println( "Device[" + i + "]: " +
-			    infos[i].getName() );
-      } catch (MidiUnavailableException mue) {
-	mue.printStackTrace();
+      if (infos[i].getName().startsWith("ALSA MIDI")) {
+	midiports.add(infos[i]);
       }
+    }
+
+    if (midiports.size() == 0) {
+      System.out.println("No (ALSA) MIDI ports found.  Exiting." );
+      System.exit(1);
+    }
+
+    System.out.println("ALSA MIDI ports:");
+    Iterator i = midiports.iterator();
+    while (i.hasNext()) {
+      System.out.println ("  " + (MidiDevice.Info)i.next());
     }
 
     System.exit(0);
