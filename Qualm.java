fdd689a87ddfbd19942d42f1cda1d605aa1e8d49
Stephen Peters <portnoy@portnoy.org>
2004-01-13 00:19:05 +0000

    Added simple mapping from client numbers to device names.

--- Qualm.java
+++ Qualm.java
@@ -2,9 +2,34 @@ package qualm;
 
 import javax.sound.midi.*;
 import java.util.*;
+import java.io.*;
 
 public class Qualm {
-  
+
+  public static Map parseALSAClients() {
+    Map ret = new HashMap();
+    String filename = "/proc/asound/seq/clients";
+    try {
+      BufferedReader br = 
+	new BufferedReader( new FileReader( filename ));
+      String lin = br.readLine();
+      while ( lin != null ) {
+	if (lin.startsWith("Client") && !lin.startsWith("Client info")) {
+	  // get the client number and name
+	  Integer clientNum = new Integer(lin.substring(6,10).trim());
+	  String clientName = lin.substring(14);
+	  clientName = clientName.substring(0,clientName.lastIndexOf('"'));
+	  ret.put(clientNum, clientName);
+	}
+	lin = br.readLine();
+      }
+    } catch (IOException ioe) {
+      System.out.println("Couldn't read " + filename + ": " + ioe);
+      return null;
+    }
+    return ret;
+  }
+
   public static void main(String[] args) {
     MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
     if (infos.length == 0) {
@@ -25,10 +50,18 @@ public class Qualm {
       System.exit(1);
     }
 
+    Map clientMap = Qualm.parseALSAClients();
+
     System.out.println("ALSA MIDI ports:");
     Iterator i = midiports.iterator();
     while (i.hasNext()) {
-      System.out.println ("  " + (MidiDevice.Info)i.next());
+      MidiDevice.Info info = (MidiDevice.Info)i.next();
+      String dev = info.getName();
+      dev = dev.substring(dev.indexOf('(')+1);
+      dev = dev.substring(0, dev.lastIndexOf( ':' ));
+      Integer cNum = new Integer(dev);
+      System.out.println ("  " + info.getName() + " [" + clientMap.get(cNum)
+			  +"]");
     }
 
     System.exit(0);
