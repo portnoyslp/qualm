167e1e8d26b52872e8e3671e4028ec4ad6f8de15
Stephen Peters <portnoy@portnoy.org>
2004-01-16 16:57:35 +0000

    Update for control changes

--- Trigger.java
+++ Trigger.java
@@ -9,66 +9,68 @@ public class Trigger {
 
   public static Trigger createNoteOnTrigger( int ch, int note ) { 
     Trigger t = new Trigger();
-    t.type = NOTE_ON;
+    t.type = ShortMessage.NOTE_ON;
     t.channel = ch;
-    t.extra = note;
+    t.extra1 = note;
     return t;
   }
   public static Trigger createNoteOffTrigger( int ch, int note ) {  
     Trigger t = new Trigger();
-    t.type = NOTE_OFF;
+    t.type = ShortMessage.NOTE_OFF;
     t.channel = ch;
-    t.extra = note;
+    t.extra1 = note;
     return t;
   }
-  public static Trigger createFootTrigger( int ch ) {  
+  public static Trigger createControlTrigger( int ch, int ctrl, int thresh ) {  
     Trigger t = new Trigger();
-    t.type = FOOT;
+    t.type = ShortMessage.CONTROL_CHANGE;
     t.channel = ch;
+    t.extra1 = ctrl;
+    t.extra2 = thresh;
     return t;
   }
   public static Trigger createClearTrigger( int ch, int duration ) {  
     Trigger t = new Trigger();
     t.type = CLEAR;
     t.channel = ch;
-    t.extra = duration;
+    t.extra1 = duration;
     return t;
   }
 
   public void setDelay(int d) { delay=d; }
   public int getDelay() { return delay; }
 
-  private String[] names = { "NoteOn", "NoteOff", "Foot", "Clear" };
+  private String[] names = { "NoteOn", "NoteOff", "Control", "Clear" };
   public String toString() {
-    return "trig[" + names[type-1] + "/" + channel + "/" + extra + "]";
+    return "trig[" + names[type-1] + "/" + channel + "/" + extra1 + "]";
   }
 
   public boolean match(MidiMessage m) {
     if (m instanceof ShortMessage) {
       ShortMessage sm = (ShortMessage)m;
-      if (type == NOTE_ON && 
-	  sm.getCommand()==sm.NOTE_ON &&
-	  channel == sm.getChannel() &&
-	  extra == sm.getData1() )
-	return true;
-      if (type == NOTE_OFF && 
-	  sm.getCommand()==sm.NOTE_OFF &&
-	  channel == sm.getChannel() &&
-	  extra == sm.getData1() )
+      // shortcut test
+      if (type != sm.getCommand()) 
+	return false;
+
+      // check for bad channel or first data byte
+      if (channel != sm.getChannel() || extra1 != sm.getData1())
+	return false;
+
+      // other checks.
+      if (type == ShortMessage.NOTE_ON || 
+	  type == ShortMessage.NOTE_OFF ||
+	  type == ShortMessage.CONTROL_CHANGE && extra2 <= sm.getData2())
 	return true;
     }
-    
     return false;
   }
   
-  public static int NOTE_ON = 1;
-  public static int NOTE_OFF = 2;
-  public static int FOOT = 3;
-  public static int CLEAR = 4;
+  public static int CLEAR = -1;
 
   protected int delay;
   protected int type;
   protected int channel;
-  protected int extra;
+  protected int extra1;
+  protected int extra2;
   
 }
