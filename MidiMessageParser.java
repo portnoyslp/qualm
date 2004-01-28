6492db2d765ba6f0956e7d2eaa5df3bd2b6e19f8
Stephen Peters <portnoy@portnoy.org>
2004-01-28 01:03:41 +0000

    More MIDI messages

--- MidiMessageParser.java
+++ MidiMessageParser.java
@@ -30,6 +30,33 @@ public class MidiMessageParser {
 	cStr = "PitchBend"; break;
       case ShortMessage.PROGRAM_CHANGE:
 	cStr = "ProgramChange"; break;
+      case ShortMessage.POLY_PRESSURE:
+	cStr = "PolyPressure"; break;
+      case ShortMessage.PITCH_BEND:
+	cStr = "PitchBend"; break;
+      case ShortMessage.MIDI_TIME_CODE:
+	cStr = "MidiTimeCode"; break;
+      case ShortMessage.SONG_POSITION_POINTER:
+	cStr = "SongPositionPointer"; break;
+      case ShortMessage.SONG_SELECT:
+	cStr = "SongSelect"; break;
+      case ShortMessage.TUNE_REQUEST:
+	cStr = "TuneRequest"; break;
+      case ShortMessage.TIMING_CLOCK:
+	cStr = "TimingClock"; break;
+      case ShortMessage.START:
+	cStr = "Start"; break;
+      case ShortMessage.CONTINUE:
+	cStr = "Continue"; break;
+      case ShortMessage.STOP:
+	cStr = "Stop"; break;
+      case ShortMessage.ACTIVE_SENSING:
+	cStr = "ActiveSensing"; break;
+      case ShortMessage.SYSTEM_RESET:
+	cStr = "SystemReset"; break;
+      case ShortMessage.END_OF_EXCLUSIVE:
+	cStr = "EndOfExclusive"; break;
+
       }
       return "[" + cStr + " chan:" + (channel+1) +
 	" d1:" + d1 + " d2:" + d2 + "]";
