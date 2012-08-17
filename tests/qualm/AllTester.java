654643020a7adbdcbbf80b1adc9fdde3486eb3f6
Stephen Peters <portnoy@portnoy.org>
2012-08-17 13:03:05 -0400

    Remove EventMapTester.  AllTester now replaced with a fileset in build.xml

--- tests/qualm/AllTester.java
+++
@@ -1,9 +0,0 @@
-package qualm;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses( { CueTest.class, EventMapperTest.class, EventTemplateTest.class })
-public class AllTester { }
