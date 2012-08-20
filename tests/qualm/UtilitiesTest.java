23ba381e2b9cd58155db6d1ce63a40c5df8189ee
Stephen Peters <portnoy@portnoy.org>
2012-08-20 16:46:40 -0400

    Update to Utilities test.

--- tests/qualm/UtilitiesTest.java
+++ tests/qualm/UtilitiesTest.java
@@ -10,20 +10,31 @@ public class UtilitiesTest {
     assertEquals(60, Utilities.noteNameToMidi("c4"));
   }
 
+  @Test
   public void parseCaseInsensitive() {
     assertEquals(60, Utilities.noteNameToMidi("C4"));
   }
 
+  @Test
   public void parseAccidentals() {
     assertEquals(61, Utilities.noteNameToMidi("C#4"));
     assertEquals(61, Utilities.noteNameToMidi("Db4"));
   }
 
+  @Test
   public void octaveBreaks() {
     assertEquals(59, Utilities.noteNameToMidi("b3"));
     assertEquals(60, Utilities.noteNameToMidi("c4"));
   }
 
+  @Test
+  public void testFlatsWithNoBlackKeys() {
+    assertEquals(64, Utilities.noteNameToMidi("Fb4"));
+    // Should this be Cb4 or Cb3?
+    assertEquals(59, Utilities.noteNameToMidi("Cb3")); 
+  }
+
+  @Test
   public void convertToName() {
     assertEquals("C4", Utilities.midiNumberToNoteName(60));
     assertEquals("C#4", Utilities.midiNumberToNoteName(61));
