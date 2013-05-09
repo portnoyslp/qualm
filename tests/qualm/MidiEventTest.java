a6644df95901bbda8935eb32e5152376a5bec01e
Stephen Peters <portnoy@portnoy.org>
2013-05-08 23:21:06 -0400

    Add test for NoteOn templates.

--- tests/qualm/MidiEventTest.java
+++ tests/qualm/MidiEventTest.java
@@ -8,6 +8,13 @@ public class MidiEventTest {
 
   @Test
   public void noteOnEventFromTemplate() {
+    EventTemplate et = EventTemplate.noteOn( 0, "c4" );
+    MidiEvent me = new MidiEvent(et);
+    assertEquals(new MidiCommand(0,MidiCommand.NOTE_ON,60,100), me.getMidiCommand() );
+  }
+
+  @Test
+  public void noteOffEventFromTemplate() {
     EventTemplate et = EventTemplate.noteOff( 0, "c4" );
     MidiEvent me = new MidiEvent(et);
     assertEquals(new MidiCommand(0,MidiCommand.NOTE_OFF,60,0), me.getMidiCommand() );
@@ -19,5 +26,4 @@ public class MidiEventTest {
     MidiEvent me = new MidiEvent(et);
     assertEquals(new MidiCommand(0,MidiCommand.CONTROL_CHANGE,7,30), me.getMidiCommand());
   }
-
 }
