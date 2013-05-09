246d6a5915668f8f9b258ff101dc3875ea144e65
Stephen Peters <portnoy@portnoy.org>
2013-05-08 23:27:50 -0400

    Add another MidiEventTest; fix error in MidiEvent logic.

--- src/qualm/MidiEvent.java
+++ src/qualm/MidiEvent.java
@@ -16,10 +16,13 @@ public class MidiEvent extends QEvent {
     switch (et.getType()) {
     case MidiCommand.NOTE_ON:
       cmd = new MidiCommand(et.channel(),et.getType(),(byte)et.getExtra1(),(byte)100);
+      break;
     case MidiCommand.NOTE_OFF:
       cmd = new MidiCommand(et.channel(),et.getType(),(byte)et.getExtra1(),(byte)0);
+      break;
     case MidiCommand.CONTROL_CHANGE:
       cmd = new MidiCommand(et.channel(),et.getType(),(byte)et.getExtra1(),(byte)et.getExtra2());
+      break;
     }
     if (cmd == null)
       throw new RuntimeException("Could not recognize template type " + et.getTypeDesc());
